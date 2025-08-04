c6f319c574c1126ea3f9b86464b601110ab5f542
mspsitedeveloper <ahmadmahdavi188@yahoo.com>
2025-08-04 15:47:01 +0330

    start app blog

--- config/settings.py
+++ config/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # local 
-    
-    'post',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- post/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import path , include
-from .views import HomePage
-urlpatterns = [
-    path('', HomePage.as_view(),name="home"),
-]
